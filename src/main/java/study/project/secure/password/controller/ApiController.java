805be6c9c50526468894ec1a390a26ed8575cd17
Luna Ribeiro Alves dos Santos <luna.as@itaipuparquetec.org.br>
2025-05-15 15:23:10 -0300

    feat: implement FailureResponse, PasswordService and ApiController before starting password validations

--- src/main/java/study/project/secure/password/controller/ApiController.java
+++ src/main/java/study/project/secure/password/controller/ApiController.java
@@ -1,6 +1,5 @@
 package study.project.secure.password.controller;
 
-import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
